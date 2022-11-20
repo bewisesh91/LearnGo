97053ecb37497876ed431f9190e0fa24e1221428
Seunghyun Mun (문승현) <seunghyun.m@bluehole.net>
2022-11-20 16:03:54 +0900

    learn go

--- main.go
+++ main.go
@@ -1,89 +1,41 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type requestResult struct {
-	url    string
-	status string
-}
-
-var errRequestFailed = errors.New("Request failed")
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-	results := make(map[string]string)
-	c := make(chan requestResult)
-
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.instagram.com/",
-		"https://www.naver.com/",
-	}
-
-	for _, url := range urls {
-		// Goroutines 적용 전
-		// result := "OK"
-		// err := hitURL(url)
-		// if err != nil {
-		// 	result = "FAILED"
-		// }
-		// results[url] = result
-
-		// Goroutines 적용 후
-		go hitURL(url, c)
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-
-	for url, status := range results {
-		fmt.Println(url, status)
-	}
-
-	// for url, result := range results {
-	// 	fmt.Println(url, result)
-	// }
-
-	// c := make(chan bool)
-	// people := [2]string{"bewise", "sh"}
-	// for _, person := range people {
-	// 	go isSmart(person, c)
-	// }
-	// result := <-c
-	// fmt.Println(result)
+	getPages()
 }
 
-// func isSmart(person string, c chan bool) {
-// 	time.Sleep(time.Second * 5)
-// 	c <- true
-// }
+func getPages() int {
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+	defer res.Body.Close()
 
-// Goroutines 적용 전
-// func hitURL(url string) error {
-// 	fmt.Println("Checking:", url)
-// 	response, err := http.Get(url)
-// 	if err != nil || response.StatusCode >= 400 {
-// 		return errRequestFailed
-// 	}
-// 	return nil
-// }
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
 
-// Goroutins CPU 수에 따라 적용할 수 있는 방법이 다름
-// Goroutines 적용 후
-func hitURL(url string, c chan<- requestResult) {
-	fmt.Println("Checking:", url)
-	response, err := http.Get(url)
-	status := "OK"
-	if err != nil || response.StatusCode >= 400 {
-		c <- requestResult{url: url, status: "FAILED"}
-	} else {
-		c <- requestResult{url: url, status: status}
+	fmt.Println(doc)
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)
 	}
 }
